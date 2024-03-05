5a76dbcf76933563a8971a9648748c9cf9ce6231
rileyentry <rileyentry@gmail.com>
2024-03-04 19:38:42 -0500

    First Commit

---
+++ external.js
@@ -0,0 +1,10 @@
+/* Navigation--------------------------*/
+
+function myNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+  x.className = "topnav";
+  }
+}
